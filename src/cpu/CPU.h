10946a126e53b7b61864e9c63b09f8a9f7361ac1
danielhammerl <daniel.hammerl99@gmx.de>
2022-11-10 16:50:01 +0100

    update

--- src/cpu/CPU.h
+++ src/cpu/CPU.h
@@ -18,7 +18,7 @@ public:
     bool run();
 
     const uint16_t RPC = 1, R00 = 2, R01 = 3, R02 = 4, R03 = 5, R04 = 6, R05 = 7,
-            R06 = 8, R07 = 9, R08 = 10, R09 = 11, RSP = 32;
+            R06 = 8, R07 = 9, R08 = 10, R09 = 11, R10 = 12, RSP = 32;
 
     const uint8_t LOAD = 1, STORE = 2, SET = 3, LOADH = 4, STOREH = 5, ADD = 6,
             SUB = 7, CJUMP = 8, MOV = 9, CJUMPI = 10;
@@ -42,6 +42,7 @@ private:
             {R07, Register("R07")},
             {R08, Register("R08")},
             {R09, Register("R09")},
+            {R10, Register("R10")},
             {RSP, Register("RSP")},
     };
 
